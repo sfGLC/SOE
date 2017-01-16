88bca6cee24b2b0ac69e670488aff609f24c0d94
sfGLC <sfan@glc.org>
2017-01-16 16:57:52 -0500

    firstcommit

---
+++ RestSOE1/RestSOE1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Server;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.SOESupport;
+
+
+//TODO: sign the project (project properties > signing tab > sign the assembly)
+//      this is strongly suggested if the dll will be registered using regasm.exe <your>.dll /codebase
+
+
+namespace RestSOE1
+{
+    [ComVisible(true)]
+    [Guid("615ad9f4-9ca0-4bca-ae49-de78181c0d97")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ServerObjectExtension("MapServer",//use "MapServer" if SOE extends a Map service and "ImageServer" if it extends an Image service.
+        AllCapabilities = "",
+        DefaultCapabilities = "",
+        Description = "Spatial Query REST SOE Sample",
+        DisplayName = "RestSOE1",
+        Properties = "FiledName=PRIMARY_;LayerName=veg",
+        SupportsREST = true,
+        SupportsSOAP = false)]
+    public class RestSOE1 : IServerObjectExtension, IObjectConstruct, IRESTRequestHandler
+    {
+        private string soe_name;
+
+        private IPropertySet configProps;
+        private IServerObjectHelper serverObjectHelper;
+        private ServerLogger logger;
+        private IRESTRequestHandler reqHandler;
+
+        private IFeatureClass m_fcToQuery;
+        private string m_mapLayerNameToQuery;
+        private string m_mapFieldToQuery;
+
+
+        public RestSOE1()
+        {
+            soe_name = this.GetType().Name;
+            logger = new ServerLogger();
+            reqHandler = new SoeRestImpl(soe_name, CreateRestSchema()) as IRESTRequestHandler;
+        }
+
+        #region IServerObjectExtension Members
+
+        public void Init(IServerObjectHelper pSOH)
+        {
+            serverObjectHelper = pSOH;
+        }
+
+        public void Shutdown()
+        {
+            logger.LogMessage(ServerLogger.msgType.infoStandard, "Shutdown", 8000, "Custom message: Shutting down the SOE");
+            soe_name = null;
+            m_fcToQuery = null;
+            m_mapFieldToQuery = null;
+            serverObjectHelper = null;
+            logger = null;
+        }
+
+        #endregion
+
+        #region IObjectConstruct Members
+
+        public void Construct(IPropertySet props)
+        {
+            configProps = props;
+
+            if (props.GetProperty("FieldName") != null)
+            {
+                m_mapFieldToQuery = props.GetProperty("FieldName") as string;
+            }
+            else
+            {
+
+            }
+            if (props.GetProperty("LayerName") != null)
+            {
+                m_mapLayerNameToQuery = props.GetProperty("LayerName") as string;
+            }
+
+        }
+
+        #endregion
+
+        #region IRESTRequestHandler Members
+
+        public string GetSchema()
+        {
+            return reqHandler.GetSchema();
+        }
+
+        public byte[] HandleRESTRequest(string Capabilities, string resourceName, string operationName, string operationInput, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            return reqHandler.HandleRESTRequest(Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties, out responseProperties);
+        }
+
+        #endregion
+
+        private RestResource CreateRestSchema()
+        {
+            RestResource rootRes = new RestResource(soe_name, false, RootResHandler);
+
+            RestOperation sampleOper = new RestOperation("sampleOperation",
+                                                      new string[] { "parm1", "parm2" },
+                                                      new string[] { "json" },
+                                                      SampleOperHandler);
+
+            rootRes.operations.Add(sampleOper);
+
+            return rootRes;
+        }
+
+        private byte[] RootResHandler(NameValueCollection boundVariables, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            responseProperties = null;
+
+            JsonObject result = new JsonObject();
+            result.AddString("hello", "world");
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+        private byte[] SampleOperHandler(NameValueCollection boundVariables,
+                                                  JsonObject operationInput,
+                                                      string outputFormat,
+                                                      string requestProperties,
+                                                  out string responseProperties)
+        {
+            responseProperties = null;
+
+            string parm1Value;
+            bool found = operationInput.TryGetString("parm1", out parm1Value);
+            if (!found || string.IsNullOrEmpty(parm1Value))
+                throw new ArgumentNullException("parm1");
+
+            string parm2Value;
+            found = operationInput.TryGetString("parm2", out parm2Value);
+            if (!found || string.IsNullOrEmpty(parm2Value))
+                throw new ArgumentNullException("parm2");
+
+            JsonObject result = new JsonObject();
+            result.AddString("parm1", parm1Value);
+            result.AddString("parm2", parm2Value);
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+
+    }
+}
