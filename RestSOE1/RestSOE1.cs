0158e951248a18e65e5509033fb3a18581f4b690
sfGLC <sfan@glc.org>
2017-01-17 16:01:13 -0500

    follow tutorial

--- RestSOE1/RestSOE1.cs
+++ RestSOE1/RestSOE1.cs
@@ -89,6 +89,48 @@ namespace RestSOE1
             {
                 m_mapLayerNameToQuery = props.GetProperty("LayerName") as string;
             }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+            try
+            {
+                IMapServer3 mapServer = (IMapServer3)serverObjectHelper.ServerObject;
+                string mapName = mapServer.DefaultMapName;
+                IMapLayerInfo layerInfo;
+                IMapLayerInfos layerInfos = mapServer.GetServerInfo(mapName).MapLayerInfos;
+
+                int c = layerInfos.Count;
+                int layerIndex = 0;
+                for (int i = 0; i < c; i++)
+                {
+                    layerInfo = layerInfos.get_Element(i);
+                    if (layerInfo.Name == m_mapLayerNameToQuery)
+                    {
+                        layerIndex = i;
+                        break;
+                    }
+                }
+
+                IMapServerDataAccess dataAccess = (IMapServerDataAccess)mapServer;
+
+                m_fcToQuery = (IFeatureClass)dataAccess.GetDataSource(mapName, layerIndex);
+                if (m_fcToQuery == null)
+                {
+                    logger.LogMessage(ServerLogger.msgType.error, "Construct", 8000, "SOE custom error: Layer name not found.");
+                    return;
+                }
+                if (m_fcToQuery.FindField(m_mapFieldToQuery) == -1)
+                {
+                    logger.LogMessage(ServerLogger.msgType.error, "Construct", 8000, "SOE custom error: Field not found in layer.");
+                }
+                
+            }
+            catch
+            {
+                logger.LogMessage(ServerLogger.msgType.error, "Construct", 8000, "SOE custom error: Could not get the feature layer.");
+            }
+
 
         }
 
@@ -112,12 +154,12 @@ namespace RestSOE1
         {
             RestResource rootRes = new RestResource(soe_name, false, RootResHandler);
 
-            RestOperation sampleOper = new RestOperation("sampleOperation",
-                                                      new string[] { "parm1", "parm2" },
+            RestOperation spatialQueryOper = new RestOperation("SpatialQuery",
+                                                      new string[] { "location", "distance" },
                                                       new string[] { "json" },
                                                       SampleOperHandler);
 
-            rootRes.operations.Add(sampleOper);
+            rootRes.operations.Add(spatialQueryOper);
 
             return rootRes;
         }
@@ -127,7 +169,7 @@ namespace RestSOE1
             responseProperties = null;
 
             JsonObject result = new JsonObject();
-            result.AddString("hello", "world");
+            //result.AddString("hello", "world");
 
             return Encoding.UTF8.GetBytes(result.ToJson());
         }
@@ -157,6 +199,100 @@ namespace RestSOE1
             return Encoding.UTF8.GetBytes(result.ToJson());
         }
 
+        private byte[] SpatialQueryOperationHandler(NameValueCollection boundVariables,
+                                                    JsonObject operationInput,
+                                                    string outputFormat,
+                                                    string requestProperties,
+                                                    out string responseProperties)
+        {
+            responseProperties = null;
+            JsonObject jsonPoint;
+            if (!operationInput.TryGetJsonObject("location", out jsonPoint))
+            {
+                throw new ArgumentNullException("location");
+            }
+            IPoint location = Conversion.ToGeometry(jsonPoint, esriGeometryType.esriGeometryPoint) as IPoint;
+            if (location == null)
+            {
+                throw new ArgumentNullException("SpatialQueryREST: invalid location", "location");
+            }
 
+            double ? distance;
+            if (!operationInput.TryGetAsDouble("distance", out distance)||!distance.HasValue)
+            {
+                throw new ArgumentNullException("SpatialQueryREST: invalid distance", "distance");
+            }
+            byte[] result = QueryPoint(location, distance.Value);
+            return result;
+        }
+
+        private byte[] QueryPoint(ESRI.ArcGIS.Geometry.IPoint location, double distance)
+        {
+            if (distance <= 0)
+            {
+                throw new ArgumentOutOfRangeException("distance");
+            }
+
+            ITopologicalOperator topologicalOperator = (ESRI.ArcGIS.Geometry.ITopologicalOperator) location;
+            IGeometry queryGeometry = topologicalOperator.Buffer(distance);
+            ISpatialFilter spatialFilter = new ESRI.ArcGIS.Geodatabase.SpatialFilter();
+            spatialFilter.Geometry = queryGeometry;
+            spatialFilter.SpatialRel = ESRI.ArcGIS.Geodatabase.esriSpatialRelEnum.esriSpatialRelIntersects;
+            spatialFilter.GeometryField = m_fcToQuery.ShapeFieldName;
+            IFeatureCursor resultsFeatureCursor = m_fcToQuery.Search(spatialFilter, true);
+
+            topologicalOperator = (ESRI.ArcGIS.Geometry.ITopologicalOperator)queryGeometry;
+            int classFieldIndex = m_fcToQuery.FindField(m_mapFieldToQuery);
+
+            Dictionary<string, double> summaryStatsDictionary = new Dictionary<string, double>();
+            List<JsonObject> jsonGeometreis = new List<JsonObject>();
+
+            IFeature resultsFeature = null;
+            while ((resultsFeature = resultsFeatureCursor.NextFeature()) != null)
+            {
+                IPolygon clippedResultsGeometry = (IPolygon)topologicalOperator.Intersect(resultsFeature.Shape,
+                                                                                          ESRI.ArcGIS.Geometry.esriGeometryDimension.esriGeometry2Dimension);
+                clippedResultsGeometry.Densify(0, 0);
+                JsonObject jsonClippedResultsGeometry = Conversion.ToJsonObject(clippedResultsGeometry);
+                jsonGeometreis.Add(jsonClippedResultsGeometry);
+
+                IArea area = (IArea)clippedResultsGeometry;
+                string resultsClass = resultsFeature.get_Value(classFieldIndex) as string;
+
+                if (summaryStatsDictionary.ContainsKey(resultsClass))
+                {
+                    summaryStatsDictionary[resultsClass] = (double)summaryStatsDictionary[resultsClass] + area.Area;
+                }
+                else
+                {
+                    summaryStatsDictionary[resultsClass] = area.Area;
+                }
+            }
+
+            JsonObject[] areaResultJson = CreateJsonRecords(summaryStatsDictionary) as JsonObject[];
+            JsonObject resultJsonObject = new JsonObject();
+            resultJsonObject.AddArray("geometries", jsonGeometreis.ToArray());
+            resultJsonObject.AddArray("records", areaResultJson);
+
+            byte[] result = Encoding.UTF8.GetBytes(resultJsonObject.ToJson());
+            return result;
+        }
+
+        private JsonObject[] CreateJsonRecords(Dictionary<string, double> inListDictionary)
+        {
+            JsonObject[] jsonRecordsArray = new JsonObject[inListDictionary.Count];
+            int i = 0;
+            foreach (KeyValuePair<string, double> kvp in inListDictionary)
+            {
+                string currentKey = kvp.Key.ToString();
+                string currentValue = kvp.Value.ToString();
+                JsonObject currentKeyValue = new JsonObject();
+                currentKeyValue.AddString(m_mapLayerNameToQuery, currentKey);
+                currentKeyValue.AddString("value", currentValue);
+                jsonRecordsArray.SetValue(currentKeyValue, i);
+                i++;
+            }
+            return jsonRecordsArray;
+        }
     }
 }
